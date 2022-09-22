5bd9c9ac27d9daa21b039227fdd7d704d2b02046
Dale Schumacher <dalnefre@yahoo.com>
2022-09-22 12:38:53 -0600

    extract Queue into its own module

--- ufork-wasm/src/ufork.rs
+++ ufork-wasm/src/ufork.rs
@@ -1,5 +1,7 @@
 // uFork virtual CPU
 
+use crate::queue::Queue;
+
 const QUAD_MAX: usize = 1<<12;  // 4K quad-cells
 
 pub struct Core {
@@ -40,11 +42,11 @@ impl Core {
             k_queue: Queue::new(),
         }
     }
-    fn in_heap(&self, val: Val) -> bool {
+    pub fn in_heap(&self, val: Val) -> bool {
         let raw = val.raw();
         (raw < self.quad_top.raw()) && (raw >= START.raw())
     }
-    fn addr(&self, ptr: Ptr) -> Option<usize> {
+    pub fn addr(&self, ptr: Ptr) -> Option<usize> {
         let raw = ptr.raw();
         if raw < self.quad_top.raw() {
             Some(raw)
@@ -52,16 +54,16 @@ impl Core {
             None
         }
     }
-    fn quad(&self, ptr: Ptr) -> &Quad {
+    pub fn quad(&self, ptr: Ptr) -> &Quad {
         let addr = self.addr(ptr).unwrap();
         &self.quad_mem[addr]
     }
-    fn quad_mut(&mut self, ptr: Ptr) -> &mut Quad {
+    pub fn quad_mut(&mut self, ptr: Ptr) -> &mut Quad {
         assert!(self.in_heap(ptr.val()));
         let addr = self.addr(ptr).unwrap();
         &mut self.quad_mem[addr]
     }
-    fn typeq(&self, typ: Val, val: Val) -> bool {
+    pub fn typeq(&self, typ: Val, val: Val) -> bool {
         if typ == FIXNUM_T {
             let fix = Fix::from(val);
             return fix.is_some();
@@ -92,7 +94,7 @@ impl Core {
             None => false,
         }
     }
-    fn alloc(&mut self, t: Val, x: Val, y: Val, z: Val) -> Ptr {
+    pub fn alloc(&mut self, t: Val, x: Val, y: Val, z: Val) -> Ptr {
         let mut ptr = self.quad_next;
         if self.typeq(FREE_T, ptr.val()) {
             assert!(self.gc_free_cnt > 0);
@@ -111,7 +113,7 @@ impl Core {
         quad.set_z(z);
         ptr
     }
-    fn free(&mut self, ptr: Ptr) {
+    pub fn free(&mut self, ptr: Ptr) {
         let val = ptr.val();
         assert!(self.in_heap(val));
         assert!(!self.typeq(FREE_T, val));
@@ -124,67 +126,29 @@ impl Core {
         self.quad_next = ptr;
         self.gc_free_cnt += 1;
     }
-    fn cons(&mut self, car: Val, cdr: Val) -> Ptr {
+    pub fn cons(&mut self, car: Val, cdr: Val) -> Ptr {
         self.alloc(PAIR_T, car, cdr, UNDEF)
     }
-    fn car(&self, cons: Ptr) -> Val {
+    pub fn car(&self, cons: Ptr) -> Val {
         let quad = self.quad(cons);
         if quad.t() == PAIR_T { quad.x() } else { UNDEF }
     }
-    fn cdr(&self, cons: Ptr) -> Val {
+    pub fn cdr(&self, cons: Ptr) -> Val {
         let quad = self.quad(cons);
         if quad.t() == PAIR_T { quad.y() } else { UNDEF }
     }
-    fn set_car(&mut self, cons: Ptr, car: Val) {
+    pub fn set_car(&mut self, cons: Ptr, car: Val) {
         let quad = self.quad_mut(cons);
         assert!(quad.t() == PAIR_T);
         quad.set_x(car);
     }
-    fn set_cdr(&mut self, cons: Ptr, cdr: Val) {
+    pub fn set_cdr(&mut self, cons: Ptr, cdr: Val) {
         let quad = self.quad_mut(cons);
         assert!(quad.t() == PAIR_T);
         quad.set_y(cdr);
     }
-    fn new_event(&mut self, target: Cap, message: Val) -> Ptr {
+    pub fn new_event(&mut self, target: Cap, message: Val) -> Ptr {
         self.alloc(EVENT_T, target.val(), message, NIL)
-    }
-}
-
-struct Queue { head: Ptr, tail: Ptr }
-impl Queue {
-    fn new() -> Queue {
-        Queue::init(NIL.ptr(), NIL.ptr())
-    }
-    fn init(head: Ptr, tail: Ptr) -> Queue {
-        Queue { head, tail }
-    }
-    fn empty(&self, core: &Core) -> bool {
-        !core.typeq(EVENT_T, self.head.val())
-    }
-    fn put(&mut self, core: &mut Core, event: Ptr) {
-        assert!(core.typeq(EVENT_T, event.val()));
-        assert!(core.typeq(ACTOR_T, core.quad(event).x()));
-        let quad = core.quad_mut(event);
-        quad.set_z(NIL);
-        if self.empty(core) {
-            self.head = event;
-        } else {
-            core.quad_mut(self.tail).set_z(event.val());
-        }
-        self.tail = event;
-    }
-    fn take(&mut self, core: &mut Core) -> Ptr {
-        if self.empty(core) {
-            return UNDEF.ptr();
-        }
-        let event = self.head;
-        let quad = core.quad_mut(event);
-        self.head = quad.z().ptr();
-        quad.set_z(NIL);
-        if self.empty(core) {
-            self.tail = NIL.ptr();  // empty queue
-        }
-        event
     }
 }
 
@@ -195,54 +159,54 @@ impl Quad {
     fn new(t: Val, x: Val, y: Val, z: Val) -> Quad {
         Quad { t, x, y, z }
     }
-    fn t(&self) -> Val { self.t }
-    fn x(&self) -> Val { self.x }
-    fn y(&self) -> Val { self.y }
-    fn z(&self) -> Val { self.z }
-    fn set_t(&mut self, v: Val) { self.t = v; }
-    fn set_x(&mut self, v: Val) { self.x = v; }
-    fn set_y(&mut self, v: Val) { self.y = v; }
-    fn set_z(&mut self, v: Val) { self.z = v; }
+    pub fn t(&self) -> Val { self.t }
+    pub fn x(&self) -> Val { self.x }
+    pub fn y(&self) -> Val { self.y }
+    pub fn z(&self) -> Val { self.z }
+    pub fn set_t(&mut self, v: Val) { self.t = v; }
+    pub fn set_x(&mut self, v: Val) { self.x = v; }
+    pub fn set_y(&mut self, v: Val) { self.y = v; }
+    pub fn set_z(&mut self, v: Val) { self.z = v; }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Val { raw: usize }
 impl Val {
-    fn new(raw: usize) -> Val {
+    pub fn new(raw: usize) -> Val {
         Val { raw }
     }
-    fn raw(&self) -> usize {
+    pub fn raw(&self) -> usize {
         self.raw
     }
-    fn fix(self) -> Fix {  // NOTE: consumes `self`
+    pub fn fix(self) -> Fix {  // NOTE: consumes `self`
         Fix::from(self).unwrap()
     }
-    fn ptr(self) -> Ptr {  // NOTE: consumes `self`
+    pub fn ptr(self) -> Ptr {  // NOTE: consumes `self`
         Ptr::from(self).unwrap()
     }
-    fn cap(self) -> Cap {  // NOTE: consumes `self`
+    pub fn cap(self) -> Cap {  // NOTE: consumes `self`
         Cap::from(self).unwrap()
     }
 }
 
-const UNDEF: Val        = Val { raw: 0 }; //Val::new(0); -- const generic issue...
-const NIL: Val          = Val { raw: 1 };
-const FALSE: Val        = Val { raw: 2 };
-const TRUE: Val         = Val { raw: 3 };
-const UNIT: Val         = Val { raw: 4 };
+pub const UNDEF: Val    = Val { raw: 0 }; //Val::new(0); -- const generic issue...
+pub const NIL: Val      = Val { raw: 1 };
+pub const FALSE: Val    = Val { raw: 2 };
+pub const TRUE: Val     = Val { raw: 3 };
+pub const UNIT: Val     = Val { raw: 4 };
 
-const LITERAL_T: Val    = Val { raw: 0 }; //Val::new(0);
-const TYPE_T: Val       = Val { raw: 5 };
-const EVENT_T: Val      = Val { raw: 6 };
-const OPCODE_T: Val     = Val { raw: 7 };
-const ACTOR_T: Val      = Val { raw: 8 };
-const FIXNUM_T: Val     = Val { raw: 9 };
-const SYMBOL_T: Val     = Val { raw: 10 };
-const PAIR_T: Val       = Val { raw: 11 };
-const FEXPR_T: Val      = Val { raw: 12 };
-const FREE_T: Val       = Val { raw: 13 };
+pub const LITERAL_T: Val= Val { raw: 0 }; //Val::new(0);
+pub const TYPE_T: Val   = Val { raw: 5 };
+pub const EVENT_T: Val  = Val { raw: 6 };
+pub const OPCODE_T: Val = Val { raw: 7 };
+pub const ACTOR_T: Val  = Val { raw: 8 };
+pub const FIXNUM_T: Val = Val { raw: 9 };
+pub const SYMBOL_T: Val = Val { raw: 10 };
+pub const PAIR_T: Val   = Val { raw: 11 };
+pub const FEXPR_T: Val  = Val { raw: 12 };
+pub const FREE_T: Val   = Val { raw: 13 };
 
-const START: Val        = Val { raw: 14 };
+pub const START: Val    = Val { raw: 14 };
 
 const MSK_RAW: usize    = 0xC000_0000;
 const DIR_RAW: usize    = 0x8000_0000;
@@ -251,10 +215,10 @@ const OPQ_RAW: usize    = 0x4000_0000;
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Fix { num: isize }
 impl Fix {
-    fn new(num: isize) -> Fix {
+    pub fn new(num: isize) -> Fix {
         Fix { num }
     }
-    fn from(val: Val) -> Option<Fix> {
+    pub fn from(val: Val) -> Option<Fix> {
         let raw = val.raw();
         if (raw & DIR_RAW) != 0 {
             let num = ((raw << 1) as isize) >> 1;
@@ -263,10 +227,10 @@ impl Fix {
             None
         }
     }
-    fn val(self) -> Val {  // NOTE: consumes `self`
+    pub fn val(self) -> Val {  // NOTE: consumes `self`
         Val::new(self.num as usize | DIR_RAW)
     }
-    fn num(&self) -> isize {
+    pub fn num(&self) -> isize {
         self.num
     }
 }
@@ -274,10 +238,10 @@ impl Fix {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Ptr { raw: usize }
 impl Ptr {
-    fn new(raw: usize) -> Ptr {
+    pub fn new(raw: usize) -> Ptr {
         Ptr { raw: (raw & !MSK_RAW) }
     }
-    fn from(val: Val) -> Option<Ptr> {
+    pub fn from(val: Val) -> Option<Ptr> {
         let raw = val.raw();
         if (raw & MSK_RAW) == 0 {
             Some(Ptr::new(raw))
@@ -285,10 +249,10 @@ impl Ptr {
             None
         }
     }
-    fn val(self) -> Val {  // NOTE: consumes `self`
+    pub fn val(self) -> Val {  // NOTE: consumes `self`
         Val::new(self.raw)
     }
-    fn raw(&self) -> usize {
+    pub fn raw(&self) -> usize {
         self.raw
     }
 }
@@ -296,10 +260,10 @@ impl Ptr {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Cap { raw: usize }
 impl Cap {
-    fn new(raw: usize) -> Cap {
+    pub fn new(raw: usize) -> Cap {
         Cap { raw: (raw & !MSK_RAW) }
     }
-    fn from(val: Val) -> Option<Cap> {
+    pub fn from(val: Val) -> Option<Cap> {
         let raw = val.raw();
         if (raw & MSK_RAW) == OPQ_RAW {
             Some(Cap::new(raw))
@@ -307,10 +271,10 @@ impl Cap {
             None
         }
     }
-    fn val(self) -> Val {  // NOTE: consumes `self`
+    pub fn val(self) -> Val {  // NOTE: consumes `self`
         Val::new(self.raw | OPQ_RAW)
     }
-    fn raw(&self) -> usize {
+    pub fn raw(&self) -> usize {
         self.raw
     }
 }
