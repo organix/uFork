627b21ac539240fe263f8f009006d9dba0f57428
Dale Schumacher <dalnefre@yahoo.com>
2023-02-09 12:19:49 -0700

    remove `wee_alloc` dependency

--- ufork-wasm/src/lib.rs
+++ ufork-wasm/src/lib.rs
@@ -8,12 +8,6 @@ pub mod device;
 
 use crate::ufork::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // FIXME: `use js_sys;` instead?
 extern crate js_sys;
 extern crate web_sys;
