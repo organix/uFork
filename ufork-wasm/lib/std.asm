a941ba86fd25fee5dc533e1a126b2aa62e61c0b0
James Diacono <james@diacono.com.au>
2023-04-10 17:41:51 +1000

    std.asm explicit k

--- ufork-wasm/lib/std.asm
+++ ufork-wasm/lib/std.asm
@@ -5,56 +5,44 @@ commit:
     end commit
 
 send_0:
-    send 0
-    ref commit
+    send 0 commit
 
 cust_send:
-    msg 1
-    ref send_0
+    msg 1 send_0
 
 rv_self:
-    my self
-    ref cust_send
+    my self cust_send
 
 rv_undef:
-    push #?
-    ref cust_send
+    push #? cust_send
 
 rv_nil:
-    push #nil
-    ref cust_send
+    push #nil cust_send
 
 rv_false:
-    push #f
-    ref cust_send
+    push #f cust_send
 
 rv_true:
-    push #t
-    ref cust_send
+    push #t cust_send
 
 rv_unit:
-    push #unit
-    ref cust_send
+    push #unit cust_send
 
 rv_zero:
-    push 0
-    ref cust_send
+    push 0 cust_send
 
 rv_one:
-    push 1
-    ref cust_send
+    push 1 cust_send
 
 resend:
     msg 0
-    my self
-    ref send_0
+    my self send_0
 
 release:
     end release
 
 release_0:
-    send 0
-    ref release
+    send 0 release
 
 stop:
     end stop
