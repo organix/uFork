dcdcc4f9e33d699377a0dfa8f3b7c4f49aefc799
James Diacono <james@diacono.com.au>
2023-04-12 09:57:02 +1000

    Development scripts

--- ufork-wasm/www/server.mjs
+++ ufork-wasm/www/server.mjs
@@ -3,33 +3,33 @@ import path from "path";
 import http from "http";
 
 const mime_types = {
-	html: "text/html",
-	js: "text/javascript",
-	wasm: "application/wasm",
-	json: "application/json",
-	asm: "text/plain"
+    html: "text/html",
+    js: "text/javascript",
+    wasm: "application/wasm",
+    json: "application/json",
+    asm: "text/plain"
 };
 
 const server = http.createServer(function on_request(req, res) {
-	const file_path = path.join(process.cwd(), req.url);
-	if (!file_path.startsWith(process.cwd())) {
-		res.statusCode = 400;
-		return res.end("Escapee.");
-	}
-	return fs.readFile(file_path, function (error, buffer) {
-		if (error) {
-			res.statusCode = 404;
-			return res.end();
-		}
-		res.setHeader("Content-Type", mime_types[file_path.split(".").pop()]);
-		return res.end(buffer);
-	});
+    const file_path = path.join(process.cwd(), req.url);
+    if (!file_path.startsWith(process.cwd())) {
+        res.statusCode = 400;
+        return res.end("Escapee.");
+    }
+    return fs.readFile(file_path, function (error, buffer) {
+        if (error) {
+            res.statusCode = 404;
+            return res.end();
+        }
+        res.setHeader("Content-Type", mime_types[file_path.split(".").pop()]);
+        return res.end(buffer);
+    });
 });
 
 server.listen(7273, "localhost", function on_listening() {
-	console.log(
-		"Navigate to http://localhost:"
-		+ server.address().port
-		+ "/www/index.html"
-	);
+    console.log(
+        "Navigate to http://localhost:"
+        + server.address().port
+        + "/www/index.html"
+    );
 });
