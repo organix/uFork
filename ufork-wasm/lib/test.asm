ad38dfdfe26a2d2eb6d93b8e1790c38d3a37035e
Dale Schumacher <dalnefre@yahoo.com>
2023-04-12 16:59:18 -0600

    fixed minor bugs in `test.asm`
    The "pair" instruction build _n_ instances of `Pair_T`,
    which consumes _n_+1 elements from the stack.
    The empty stack is #?, so "pair 2" with only two elements
    on the stack gives `(1 2 . #?)`, which works just fine
    with `fib`, since it doesn't look past the 2nd arg.

--- ufork-wasm/lib/test.asm
+++ ufork-wasm/lib/test.asm
@@ -18,10 +18,9 @@ boot:                   ; {caps}
     msg 0               ; n {caps}
     push IO_DEV_KEY     ; n {caps} IO_DEV_KEY
     dict get            ; n IO_DEV
-    pair 2              ; (IO_DEV n)
-    push fib.beh        ; (IO_DEV n) beh
-    new 0               ; (IO_DEV n) actor
-    ref std.send_0      ;
+    push fib.beh        ; n IO_DEV beh
+    new 0               ; n IO_DEV actor
+    send 2 std.commit
 
 .export
     boot
