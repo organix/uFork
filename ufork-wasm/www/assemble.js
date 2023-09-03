6e1abd1e3c8319285856e0c9a84202896fcd0045
Dale Schumacher <dalnefre@yahoo.com>
2023-09-02 21:29:01 -0600

    restrict allowable Names in uFork ASM

--- ufork-wasm/www/assemble.js
+++ ufork-wasm/www/assemble.js
@@ -25,9 +25,10 @@ const rx_token_raw = tag_regexp `
   | ( \u0020+ )
   | ( ; .* )
   | (
-        [ a-z A-Z ~ _ ]
-        [ a-z A-Z ~ 0-9 \- _ ]*
-        [ \? ! ]?
+        [ a-z A-Z ]
+        (?:
+            [ \- _ ]? [ 0-9 a-z A-Z ]
+        )*
     )
   | (
         #
