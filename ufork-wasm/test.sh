51c409b266a61dd5d9858a53bd6ed4185720aac7
Dale Schumacher <dalnefre@yahoo.com>
2023-11-20 13:18:45 -0700

    build library and run tests sequentially

--- ufork-wasm/test.sh
+++ ufork-wasm/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Run the test runners in parallel.
+# Run the test runners in sequentially.
 
+cd ../ufork-rust/
+cargo test --lib
+
+cd ../ufork-wasm/
 deno run \
     --allow-read=. \
     run_asm_tests.js \
     examples \
-    lib \
-    &
-
-cd ../ufork-rust/ && cargo test --lib &
-
-wait %1 %2
+    lib
