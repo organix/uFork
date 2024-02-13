3187fcc5ab71740218d9938d8d637f026ed87c6a
Dale Schumacher <dalnefre@yahoo.com>
2024-02-13 16:02:04 -0700

    use `svg_drawing` state machine to generate HTML content

--- vm/js/svg_dev.js
+++ vm/js/svg_dev.js
@@ -1,13 +1,20 @@
 // The SVG device draws into an SVG element, based on the commands it receives.
 
+import svg_drawing from "https://ufork.org/lib/svg_drawing.js";
+
 const svg_key = 101; // from dev.asm
 
 function svg_dev(core, make_ddev, svg_element) {
+    function svg_append(html) {
+        svg_element.innerHTML += html;//`<text x=10 y=10>${html}</text>`;
+    }
+
+    const on_code = svg_drawing(svg_append);
     const ddev = make_ddev(function on_event_stub(ptr) {
         const event_stub = core.u_read_quad(ptr);
         const event = core.u_read_quad(event_stub.y);
-        const message = core.u_fix_to_i32(event.y);
-        svg_element.innerHTML += `<text x=10 y=10>${message}</text>`;
+        const code = core.u_fix_to_i32(event.y);
+        on_code(code);
     });
     const svg_proxy = ddev.h_reserve_proxy();
     core.h_install([[core.u_fixnum(svg_key), svg_proxy]]);
