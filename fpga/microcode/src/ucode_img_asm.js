5b21da71056e452c58734604a7248fdf77adc3da
Zarutian <zarutian@gmail.com>
2024-02-17 14:57:31 +0000

    Create ucode_img_asm.js

---
+++ fpga/microcode/src/ucode_img_asm.js
@@ -0,0 +1,18 @@
+// @ts-check js
+/**
+ * @use JSDoc
+ * @overview This is the micro code image
+ * @author Zarutian
+ */
+
+import { makeAssembler } from "../util/masm.js";
+
+export const defineInstructionset = (asm) => {
+};
+
+export const makeUcodeImage = (opts) => {
+};
+
+export default {
+  makeUcodeImage,
+};
