6b2097275c647a6e9c9675f9ca515a59101cded5
Zarutian <zarutian@gmail.com>
2024-02-19 13:20:02 +0000

    Create uFork.js
    This file might get quite big.

---
+++ fpga/microcode/src/uFork.js
@@ -0,0 +1,15 @@
+// @ts-check js
+/**
+ * @use JSDoc
+ * @overview Implements the uFork instructionset in microcode
+ * @author Zarutian
+ */
+
+export const uFork_instrHandling = (asm, opts) => {
+
+  return asm;
+};
+
+export default {
+  uFork_instrHandling
+};
