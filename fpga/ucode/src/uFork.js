a1c10cef6b7c38226590be5fc26c836842ac4d70
Zarutian <zarutian@gmail.com>
2024-02-26 15:38:20 +0000

    uFork alu add instruction implemented

--- fpga/ucode/src/uFork.js
+++ fpga/ucode/src/uFork.js
@@ -451,6 +451,13 @@ export const uFork = (asm, opts) => {
   dat("OR");
   dat("(JMP)", "uFork_instr_alu__common_tail");
 
+  def("uFork_instr_alu_add"); // ( kont subopcode )
+  dat("DROP");               // ( kont )
+  // todo: insert fixnum type check here for TOS and NOS items
+  dat("uFork_pop_two_fixnums2ints"); // ( kont a b )
+  dat("+");
+  dat("(JMP)", "uFork_instr_alu__common_tail");
+
 
   def("uFork_instr__subroutine_call"); // ( kont ip opcode -- )
   if (uForkSubroutines) {
