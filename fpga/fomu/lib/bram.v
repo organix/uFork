68898fd069840b6a5781603ee0fd9ee77d364dd6
Dale Schumacher <dalnefre@yahoo.com>
2024-02-26 12:28:28 -0700

    add BRAM module

---
+++ fpga/fomu/lib/bram.v
@@ -0,0 +1,52 @@
+/*
+
+iCE40 Dual-Ported Block RAM
+(derived from FPGA-TN-02002-1-7-Memory-Usage-Guide-for-iCE40-Devices.pdf)
+
+    +-----------------+
+    | bram (4kb)      |
+    |                 |
+--->|i_rd_en   i_wr_en|<---
+=A=>|i_raddr   i_waddr|<=A=
+<=D=|o_rdata   i_wdata|<=D=
+    |                 |
+ +->|i_clk            |
+ |  +-----------------+
+
+*/
+
+`default_nettype none
+
+module bram #(
+    // DATA_SZ x MEM_MAX = 4096 bits
+    parameter DATA_SZ           = 16,                           // number of bits per memory word
+    parameter ADDR_SZ           = 8,                            // number of bits in each address
+    parameter MEM_MAX           = (1<<ADDR_SZ)                  // maximum memory memory address
+) (
+    input                       i_clk,                          // system clock
+
+    input                       i_wr_en,                        // write request
+    input         [ADDR_SZ-1:0] i_waddr,                        // write address
+    input         [DATA_SZ-1:0] i_wdata,                        // data written
+
+    input                       i_rd_en,                        // read request
+    input         [ADDR_SZ-1:0] i_raddr,                        // read address
+    output reg    [DATA_SZ-1:0] o_rdata                         // data read
+);
+
+    // inferred block ram
+    reg [DATA_SZ-1:0] mem [0:MEM_MAX-1];
+
+    always @(posedge i_clk) begin
+
+        // write conditionally
+        if (i_wr_en) begin
+            mem[i_waddr] <= i_wdata;
+        end
+
+        // read always
+        o_rdata <= mem[i_raddr];
+
+    end
+
+endmodule
---
+++ fpga/fomu/lib/bram_tb.v
@@ -0,0 +1,48 @@
+/*
+
+Test Bench for bram.v
+
+*/
+
+`default_nettype none
+
+`include "bram_test.v"
+
+`timescale 10ns/1ns
+
+module test_bench;
+
+    // dump simulation signals
+    initial begin
+        $dumpfile("bram.vcd");
+        $dumpvars(0, test_bench);
+        #200;
+        $finish;
+    end
+
+    // generate chip clock (50MHz simulation time)
+    reg clk = 0;
+    always begin
+        #1 clk = !clk;
+    end
+
+    // start-up delay
+    reg [5:0] waiting;
+    initial waiting = 3;                                        // wait for device initialization
+    always @(posedge clk) begin
+        if (waiting) begin
+            waiting <= waiting - 1'b1;
+        end
+    end
+
+    // instantiate test fixture
+    wire running;
+    wire passed;
+    bram_test TEST (
+        .i_clk(clk),
+        .i_run(!waiting),
+        .o_running(running),
+        .o_passed(passed)
+    );
+
+endmodule
