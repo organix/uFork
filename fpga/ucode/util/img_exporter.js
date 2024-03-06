7bd54e9d74648bc9e9f91da36cfc327b4b5cc2c0
Zarutian <zarutian@gmail.com>
2024-03-06 21:09:51 +0000

    . Update img_exporter.js

--- fpga/ucode/util/img_exporter.js
+++ fpga/ucode/util/img_exporter.js
@@ -13,7 +13,7 @@ import { makeArrayFromIterator } from "./util_funcs.js";
   * @parameter {Map<Uint16, Uint16>} img - the ucode program memory image
   * @returns {Array<Uint16>} an array of uint16s
   **/
-const convert_img_to_array = (img) => {
+export const convert_img_to_array = (img) => {
     const entries = makeArrayFromIterator(img.entries());
     const result  = new Array(entries.length);
     entries.forEach(([addr, value]) => {
@@ -27,7 +27,7 @@ const convert_img_to_array = (img) => {
   * @parameter {undefined | any} opts - options bag object
   * @returns {string} the .memh contents
   **/
-const convert_img_to_memh = (img, opts) => {
+export const convert_img_to_memh = (img, opts) => {
     const arr = convert_img_to_array(img);
     return String.prototype.concat.apply("// verilog memh format\n", arr.map(
         (item) => "".concat(Number(item).toString(16), "\n")
@@ -39,7 +39,7 @@ const convert_img_to_memh = (img, opts) => {
   * @parameter {undefined | any} opts - options bag object
   * @returns {string} the wozmon hexdump
   **/
-const convert_img_to_wozmon_hex = (img, opts) => {
+export const convert_img_to_wozmon_hex = (img, opts) => {
     const entries = makeArrayFromIterator(img.entries());
     // tbd: maybe sort entries based on address?
     return String.prototype.concat.apply("", entries.map(
