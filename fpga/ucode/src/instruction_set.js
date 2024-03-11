f64debaae365756325d815c2c8f0ce12b0697330
Zarutian <zarutian@gmail.com>
2024-03-11 14:18:17 +0000

    . Update instruction_set.js

--- fpga/ucode/src/instruction_set.js
+++ fpga/ucode/src/instruction_set.js
@@ -8,33 +8,40 @@
 import { makeAssembler } from "../util/masm.js";
 import { uFork } from "./uFork.js";
 
-export const defineInstructionset = (asm) => {
+export const defineInstructionset = (asm, opts = { instrsetName: "uFork_CSM2" }) => {
   const { def } = asm;
-  def("NOP",    0x0000);
-  def("PLUS",   0x0001);
-  def("AND",    0x0002);
-  def("XOR",    0x0003);
-  def("1LBR",   0x0004);
-  def("INCR",   0x0005);
-  def("FETCH",  0x0006);
-  def("STORE",  0x0007);
-  def("DUP",    0x0008);
-  def("DROP",   0x0009);
-  def("SWAP",   0x000A);
-  def("SKZ",    0x000B);
-  def("TO_R",   0x000C);
-  def("R_FROM", 0x000D);
-  def("EXT",    0x000E);
-  def("EXIT",   0x000F);
+  switch (opts.instrsetName) {
+    case "FCPU-16 w/qmem":
+      def("QUAD_T_FETCH", 0x0010);
+      def("QUAD_X_FETCH", 0x0011);
+      def("QUAD_Y_FETCH", 0x0012);
+      def("QUAD_Z_FETCH", 0x0013);
+      def("QUAD_T_STORE", 0x0014);
+      def("QUAD_X_STORE", 0x0015);
+      def("QUAD_Y_STORE", 0x0016);
+      def("QUAD_Z_STORE", 0x0017);
+      // deliberate fallthrough to next case
+    case "FCPU-16":
+      def("NOP",    0x0000);
+      def("UMPLUS",   0x0001);
+      def("AND",    0x0002);
+      def("XOR",    0x0003);
+      def("1LBR",   0x0004);
+      def("INCR",   0x0005);
+      def("FETCH",  0x0006);
+      def("STORE",  0x0007);
+      def("DUP",    0x0008);
+      def("DROP",   0x0009);
+      def("SWAP",   0x000A);
+      def("SKZ",    0x000B);
+      def("TO_R",   0x000C);
+      def("R_FROM", 0x000D);
+      def("EXT",    0x000E);
+      def("EXIT",   0x000F);
+      break;
+  }
 
-  def("QUAD_T_FETCH", 0x0010);
-  def("QUAD_X_FETCH", 0x0011);
-  def("QUAD_Y_FETCH", 0x0012);
-  def("QUAD_Z_FETCH", 0x0013);
-  def("QUAD_T_STORE", 0x0014);
-  def("QUAD_X_STORE", 0x0015);
-  def("QUAD_Y_STORE", 0x0016);
-  def("QUAD_Z_STORE", 0x0017);
+  
   def("QUAD_ALLOCATE", 0x0018);
   def("QUAD_FREE",    0x0019);
   def("QUAD_GCSTEP",  0x001A);
