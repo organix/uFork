0162a7a64cd4fff84e7f75201ca51c96c654bc83
Zarutian <zarutian@gmail.com>
2024-03-20 01:17:25 +0000

    . Update uFork_quadmem.js

--- fpga/ucode/src/uFork_quadmem.js
+++ fpga/ucode/src/uFork_quadmem.js
@@ -4,3 +4,9 @@
  * @overview Here is implemented the software fallbacks for both quad memory and its garbage collection.
  * @author Zarutian
  **/
+
+export const uFork_quadmem_and_gc = (asm) => {
+  const { def, dat, isDefined } = asm;
+
+  return asm;
+};
