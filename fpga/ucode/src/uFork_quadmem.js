ef3ea658e7c3b8b1a640d18e4b63add8aa0352ad
Zarutian <zarutian@gmail.com>
2024-03-20 15:33:14 +0000

    . Update uFork_quadmem.js

--- fpga/ucode/src/uFork_quadmem.js
+++ fpga/ucode/src/uFork_quadmem.js
@@ -35,7 +35,29 @@ export const uFork_quadmem_and_gc = (asm) => {
       def("qram"); // ( quad_addr -- addr )
       dat("DUP", "0x6000_&", "SWAP", "4*", "OR", "qram_base", "+", "EXIT");
 
+      def("qrom"); // ( quad_addr -- addr )
+      dat("4*", "qrom_base", "+", "EXIT");
 
+      def("qramt", "qram");
+      def("qramx"); dat("qram", "1+", "EXIT");
+      def("qramy"); dat("qram", "2+", "EXIT");
+      def("qramz"); dat("qram", "3+", "EXIT");
+      
+      def("qramt@"); // ( qaddr -- item )
+      dat("qramt", "@", "EXIT");
+      
+      def("qramx@"); dat("qramx", "@", "EXIT");
+      def("qramy@"); dat("qramy", "@", "EXIT");
+      def("qramz@"); dat("qramz", "@", "EXIT");
+      def("qramt!"); dat("qramt", "!", "EXIT");
+      def("qramx!"); dat("qramx", "!", "EXIT");
+      def("qramy!"); dat("qramy", "!", "EXIT");
+      def("qramz!"); dat("qramz", "!", "EXIT");
+      def("qromt@"); dat("qrom", "@", "EXIT");
+      def("qromx@"); dat("qrom", "1+", "@", "EXIT");
+      def("qromy@"); dat("qrom", "2+", "@", "EXIT");
+      def("qromz@"); dat("qrom", "3+", "@", "EXIT");
+    } else {
   
   return asm;
 };
