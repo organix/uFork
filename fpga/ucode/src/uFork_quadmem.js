e0bc9654124ac13dbe74e4c9777d1a62c1e46037
Zarutian <zarutian@gmail.com>
2024-03-22 22:30:59 +0000

    . Update uFork_quadmem.js

--- fpga/ucode/src/uFork_quadmem.js
+++ fpga/ucode/src/uFork_quadmem.js
@@ -257,5 +257,13 @@ export const uFork_quadmem_and_gc = (asm) => {
       dat("DROP");
       dat("uFork_gc_last", "2+", "uFork_gc_first", "gcMem!");
       dat("(JMP)", "uFork_gc_idle_l0");
+
+      def("uFork_gc_mark"); // ( phase -- )
+      dat("uFork_gc_nextOfScanque", "DUP", "uFork_()", "(BRZ)", "uFork_gc_mark_l0");
+      dat("DROP", "(JMP)", "uFork_gc_idle_l0");
+      def("uFork_gc_mark_l0"); // ( phase qa )
+      dat("uFork_gc_scan_quad"); // ( phase )
+      dat("DUP", "4&", "(BRNZ)", "uFork_gc_mark"); // stop-the-world condition
+      dat("DROP", "EXIT");
   return asm;
 };
