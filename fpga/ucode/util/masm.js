77445070dae648aaee62264ff64e6473383bd0f1
Zarutian <zarutian@gmail.com>
2024-04-07 03:19:11 +0000

    . Update masm.js

--- fpga/ucode/util/masm.js
+++ fpga/ucode/util/masm.js
@@ -340,7 +340,7 @@ export const makeAssembler = (opts) => {
     // iterate through the image, looking for promises
     image.forEach((val, addr) => {
       if ((typeof val) == "object") {
-        if ((val.promise != undefined) || (val instanceof Promise) {
+        if ((val.promise != undefined) || (val instanceof Promise)) {
           done_reject(new Error(`addr ${addr} has a promise and no concrete value`));
         }
       }
