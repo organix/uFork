56c17e277fb96ca6ef2ca3e8c1b2045f58c4fd0f
Zarutian <zarutian@gmail.com>
2024-04-17 14:15:14 +0000

    . Update masm.js

--- fpga/ucode/util/masm.js
+++ fpga/ucode/util/masm.js
@@ -3,3 +3,12 @@
  * @overview This is a small macro assembler
  * @author Zarutian
  **/
+
+export const makeAssembler = (opts) => {
+  opts = (opts == undefined) ? {} : opts;
+  const cellsize = (opts.cellsize == undefined) ? 16 : opts.cellsize ;
+  const fullcellBitmask = makeBitmask(cellsize);
+  const syms = (opts.symbols == undefined) ? new Map() : opts.symbols ;
+  let curr_addr = (opts.origin  == undefined) ? 0x0000 : opts.origin ;
+  const image = (opts.image == undefined) ? new Map() : opts.image ;
+}
