72d0c7a207a9ec7e47a354266f87ab67ccf72832
Zarutian <zarutian@gmail.com>
2024-04-24 14:39:44 +0000

    . Update spi.v

--- fpga/fomu/lib/spi.v
+++ fpga/fomu/lib/spi.v
@@ -1,4 +1,18 @@
-// Serial Peripheral Interface virtual component
+/*
+  Serial Peripheral Interface master virtual component
+
+    +-----------------+
+    | spi master      |
+    |                 |
+--->|i_spi_en   o_SCLK|--->
+=8=>|i_data     o_MOSI|--->
+<=8=|o_data     i_MISO|<---
+<---|o_ready      o_SS|--->
+    |                 |
+ +->|i_clk            |
+ |  +-----------------+
+
+*/
 
 module
 
