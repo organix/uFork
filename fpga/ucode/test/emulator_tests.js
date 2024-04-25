b77810d81c49f514f3f188f25b7cceba4e02fff2
Zarutian <zarutian@gmail.com>
2024-04-25 17:34:44 +0000

    . Update emulator_tests.js

--- fpga/ucode/test/emulator_tests.js
+++ fpga/ucode/test/emulator_tests.js
@@ -16,7 +16,7 @@ const require = (module_specifier) => {
   }
 };
 
-const { test } = require("npm:ava");
+const { test } = await require("npm:ava");
 
 // ava docu: https://github.com/avajs/ava/tree/2e0c2b1cef779e1c092eb60f0a9558bb9cf4c848/docs
 /*
@@ -26,5 +26,18 @@ todo:
   3. write a test to test quad memory
   4. write a test to test a small ucode image
 */
+/* template:
 test("name of test", t => {
+});
+*/
+// note this only works for uCode cpu version 2.2
+const common_setup = async (opts) => {
+  const {
+    makeMemory,
+    makeQuadMemory,
+    makeDebugIOStub,
+    makeEmulator_uFork_SM2v2,
+  } = await require("../util/emulator.js");
+}
+test("uCode cpu 2v2 NOP instr", async t => {
 });
