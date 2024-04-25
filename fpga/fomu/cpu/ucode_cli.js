9073d1c7c77cfdcd15e39fbdbb24188c2aade61d
James Diacono <james@diacono.com.au>
2024-04-25 10:01:48 +1000

    extract ucode_disasm.js

--- fpga/fomu/cpu/ucode_cli.js
+++ fpga/fomu/cpu/ucode_cli.js
@@ -7,57 +7,26 @@
 
 //      deno run ucode_cli.js <ucode.f >ucode_rom.mem
 
-/*jslint deno */
+/*jslint deno, bitwise */
 
 import compile from "./ucode.js";
+import disasm from "./ucode_disasm.js";
 
 const text_encoder = new TextEncoder();
 
 // Read the entirety of STDIN as a string.
 
 new Response(Deno.stdin.readable).text().then(function (text) {
-    function disasm(code) {
-        if (code === 0x021F) {
-            return "(LIT)";
-        }
-        if (code === 0x521F) {
-            return "(CONST)";
-        }
-        let suffix = "";
-        if ((code & 0xF000) === 0x5000) {
-            code &= 0x0FFF;
-            suffix = " EXIT";
-        }
-        for (const [name, word] of Object.entries(result.words)) {
-            if (word === code) {
-                return name + suffix;
-            }
-        }
-        let text = "";
-        if (code & 0x8000) {
-            text += (code & 0x4000) ? "call" : "jump";
-            if (code & 0x3000) {
-                if (code & 0x2000) {
-                    text += (code & 0x1000 ? "_ifnz_dec" : "_ifnz_inc");
-                } else {
-                    text += "_ifzero";
-                }
-            }
-            const addr = code & 0xFFF;
-            text += "(" + addr.toString(16).padStart(3, "0") + ")";
-        }
-        return text;
-    }
-
-    const result = compile(text);
+    const {errors, words, prog} = compile(text);
 
 // Fail if there was a compilation error.
 
-    if (result?.errors?.length) {
-        result.errors.forEach(function (err) {
+    if (errors !== undefined && errors.length > 0) {
+        errors.forEach(function (err) {
             Deno.stderr.write(text_encoder.encode(
-                err.src + ":" + err.line + " " + err.error + "\n"));
-        })
+                err.src + ":" + err.line + " " + err.error + "\n"
+            ));
+        });
         return Deno.exit(1);
     }
 
@@ -68,51 +37,47 @@ new Response(Deno.stdin.readable).text().then(function (text) {
 /*
 // Dump symbol table as leading comments.
 
-    for (const [name, word] of Object.entries(result.words)) {
-        if (typeof word === "number" && (word & 0x8000 !== 0)) {
+    Object.entries(words).forEach(function ([name, word]) {
+        if (typeof word === "number" && (word & 0x8000) !== 0) {
             const word_hex = word.toString(16).padStart(4, "0");
             lines.push("// " + word_hex + ": " + name);   // symbol table entry
         }
-    }
+    });
 */
-
-    result.prog.forEach(function (word, address) {
+    prog.forEach(function (word, address) {
 
 // Occasionally include the current word's address in a comment.
 
         if (address % 16 === 0) {
-            const address_hex = address.toString(16).padStart(8, "0");
-            lines.push("// 0x" + address_hex);
+            const long_hex = address.toString(16).padStart(8, "0");
+            lines.push("// 0x" + long_hex);
         }
 
 // Include symbol-table entries as comments.
 
         const call = 0xC000 | address;
-        for (const [name, word] of Object.entries(result.words)) {
+        Object.entries(words).forEach(function ([name, word]) {
             if (word === call) {
-                const address_hex = address.toString(16).padStart(3, "0");
-                lines.push("// " + address_hex + ": " + name);   // symbol table entry
+                const hex = address.toString(16).padStart(3, "0");
+                lines.push("// " + hex + ": " + name);   // symbol table entry
             }
-        }
+        });
 
 // Encode each word as a hex string, one per line.
 
-        let line = word.toString(16).padStart(4, "0");
-        const annotation = disasm(word);
-        if (annotation) {
-            line += "  // " + annotation;
-        }
-        lines.push(line);
-
+        const short_hex = word.toString(16).padStart(4, "0");
+        const annotation = disasm(word, words);
+        lines.push(
+            annotation !== ""
+            ? short_hex + "  // " + annotation
+            : short_hex
+        );
     });
 
 // Include length as a final comment.
 
-    const length_hex = result.prog.length.toString(16).padStart(8, "0");
+    const length_hex = prog.length.toString(16).padStart(8, "0");
     lines.push("// 0x" + length_hex);
-
-    const mem = lines.join("\n") + "\n";
-    Deno.stdout.write(text_encoder.encode(mem));
-
+    Deno.stdout.write(text_encoder.encode(lines.join("\n") + "\n"));
     return Deno.exit(0);
 });
