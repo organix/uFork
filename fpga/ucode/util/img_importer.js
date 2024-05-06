6ded5917fee39cdcf7d7907c04be95ef66e087ec
Zarutian <zarutian@gmail.com>
2024-05-06 13:35:09 +0000

    . Update img_importer.js

--- fpga/ucode/util/img_importer.js
+++ fpga/ucode/util/img_importer.js
@@ -13,7 +13,10 @@ export const memh2img = (inp) => {
   const t1 = inp.split("\n"); // split into lines
   const t2 = t2.map((line) => line.slice(0, line.indexOf("//"))); // get rid of comments
   const t3 = t3.filter((line) => (line != "")); // get rid of empty lines
-  // merkill
+  let addr = 0x0000;
+  const img = new Map();
+  t3.forEach((line) => { img.set(addr, Number.parseInt(line, 16)); addr += 1; });
+  return [img, undefined];
 };
 export default {
   memh2img,
