0882bee7349661ca30fb9155ac7767a4ade54a00
James Diacono <james@diacono.com.au>
2024-09-05 16:52:13 +1000

    report Humus parsing errors

--- apps/playground/lang_hum.js
+++ apps/playground/lang_hum.js
@@ -1,9 +1,11 @@
 // Text editor support for Humus.
 
 import compile from "https://ufork.org/lib/humus.js";
+import dom from "./dom.js";
 import ed_comment from "./ed_comment.js";
 import ed_duplication from "./ed_duplication.js";
 import ed_tab from "./ed_tab.js";
+import theme from "./theme.js";
 
 const indent = "    ";
 const rx_comment = /^(\s*)(#\u0020?)/;
@@ -11,7 +13,34 @@ const comment_prefix = "# ";
 
 function highlight(element) {
     const text = element.textContent;
-    element.textContent = text;  // discard formatting from other lang packs
+    element.textContent = "";
+    const result = compile(text);
+    let position = 0;
+    result.errors.filter(function (error) {
+        return (
+            Number.isSafeInteger(error.start)
+            && Number.isSafeInteger(error.end)
+        );
+    }).sort(function (a, b) {
+        return b.start - a.start;
+    }).forEach(function (error) {
+        element.append(
+            text.slice(position, error.start),
+            dom(
+                "span",
+                {
+                    style: {
+                        borderRadius: "2px",
+                        outline: "1px solid " + theme.red
+                    },
+                    title: error.message
+                },
+                text.slice(error.start, error.end)
+            )
+        );
+        position = error.end;
+    });
+    element.append(text.slice(position));  // remnant
 }
 
 function handle_keydown(editor, event) {
@@ -21,7 +50,7 @@ function handle_keydown(editor, event) {
 }
 
 function stringify_error(error) {
-    return error.error;
+    return `[${error.line ?? "?"}:${error.column ?? "?"}] ${error.message}`;
 }
 
 export default Object.freeze({
