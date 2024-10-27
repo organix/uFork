b33f3b55446127f6e374680c854919eb6d28cb46
James Diacono <james@diacono.com.au>
2024-10-28 07:58:43 +1100

    -1ify loop.asm

--- apps/debugger/examples/loop.asm
+++ apps/debugger/examples/loop.asm
@@ -7,14 +7,17 @@ loop:
     drop 0 loop             ; no-op loop
 
 boot:
-    msg 0                   ; {caps}
-    push std.resend         ; {caps} std.resend
-    new 0                   ; {caps} a_resend
-    send 0                  ; --
-    push loop               ; loop
-    new 0                   ; a_loop
-    send 0                  ; --
-    ref std.commit
+    push #?                 ; #?
+    msg 0                   ; #? {caps}
+    push #?                 ; #? {caps} #?
+    push std.resend         ; #? {caps} #? std.resend
+    new -1                  ; #? {caps} a_resend
+    send -1                 ; --
+    push #?                 ; #?
+    push #?                 ; #? #?
+    push loop               ; #? #? loop
+    new -1                  ; #? a_loop
+    ref std.send_msg
 
 .export
     boot
