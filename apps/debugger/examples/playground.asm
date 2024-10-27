0da083a216935f777e687569f1f90fd9bba46f82
James Diacono <james@diacono.com.au>
2024-10-28 08:09:39 +1100

    -1ify playground.asm

--- apps/debugger/examples/playground.asm
+++ apps/debugger/examples/playground.asm
@@ -9,8 +9,6 @@
 .import
     std: "https://ufork.org/lib/std.asm"
     dev: "https://ufork.org/lib/dev.asm"
-;    lib: "https://ufork.org/lib/lib.asm"
-;    asm: "https://ufork.org/lib/asm.asm"
 
 ; An infinite loop will consume cycles, but no memory or events.
 
@@ -19,7 +17,7 @@ loop_forever:
 
 ; Send yourself an incrementing number forever.
 
-ticker:                     ; () <- n
+ticker:                     ; _ <- n
     msg 0                   ; n
     push 1                  ; n 1
     alu add                 ; n+1
@@ -28,32 +26,38 @@ ticker:                     ; () <- n
 
 ; Send yourself two messages for each one received.
 
-msg_bomb:                   ; () <- ()
-    my self                 ; SELF
-    send 0                  ; --
-    my self                 ; SELF
-    send 0                  ; --
+msg_bomb:                   ; _ <- _
+    push #?                 ; #?
+    my self                 ; #? SELF
+    send -1                 ; --
+    push #?                 ; #?
+    my self                 ; #? SELF
+    send -1                 ; --
     ref std.commit
 
 ; Create and activate two clones for each message received.
 
-fork_bomb:                  ; () <- ()
-    push fork_bomb          ; fork_bomb
-    new 0                   ; fork_bomb.()
-    send 0                  ; --
-    push fork_bomb          ; fork_bomb
-    new 0                   ; fork_bomb.()
-    send 0                  ; --
+fork_bomb:                  ; _ <- _
+    push #?                 ; #?
+    push #?                 ; #? #?
+    push fork_bomb          ; #? #? fork_bomb
+    new -1                  ; #? fork_bomb.#?
+    send -1                 ; --
+    push #?                 ; #?
+    push #?                 ; #? #?
+    push fork_bomb          ; #? #? fork_bomb
+    new -1                  ; #? fork_bomb.#?
+    send -1                 ; --
     ref std.commit
 
 ; Count up to a specified `limit` (forever, if `limit==#?`).
 
-count_to:                   ; (limit) <- count
-    state 1                 ; limit
+count_to:                   ; limit <- count
+    state 0                 ; limit
     typeq #fixnum_t         ; typeof(limit)==#fixnum_t
     if_not count_next       ; --
     msg 0                   ; count
-    state 1                 ; count limit
+    state 0                 ; count limit
     cmp ge                  ; count>=limit
     if std.commit           ; --
 count_next:
@@ -65,16 +69,23 @@ count_next:
 
 ;;; Hewitt Go/Stop "Unbounded Integer" Example
 ;
-; Theorem. An Actor machine can perform computations that a no λ expression, nondeterministic Turing
-; Machine or pure Logic Program can implement because there is an always-halting Actor machine that can
-; compute an integer of unbounded size (cf. [Clinger 1981]) This can be accomplished using an Actor with a
-; variable count that is initially 0 and a variable continue initially True. The computation is begun by
-; concurrently sending two requests to the Actor machine: a stop request that will return an integer and a go
-; request that will return Void. The Actor machine operates as follows: 1
-;  • When a stop request is received, return count and set continue to False for the next request received.
+; Theorem. An Actor machine can perform computations that a no λ expression,
+; nondeterministic Turing Machine or pure Logic Program can implement because
+; there is an always-halting Actor machine that can compute an integer of
+; unbounded size (cf. [Clinger 1981]).
+
+; This can be accomplished using an Actor with a variable count that is
+; initially 0 and a variable continue initially True. The computation is begun
+; by concurrently sending two requests to the Actor machine: a stop request
+; that will return an integer and a go request that will return Void. The Actor
+; machine operates as follows:
+
+;  • When a stop request is received, return count and set continue to False for
+;    the next request received.
 ;  • When a go request is received:
-;    o If continue is True, increment count by 1, send this Actor machine a go request in a hole of the region
-;      of mutual exclusion, and then return Void.
+;    o If continue is True, increment count by 1, send this Actor machine a go
+;      request in a hole of the region of mutual exclusion, and then return
+;      Void.
 ;    o If continue is False, return Void.
 
 unbounded:                  ; num <- inc | cust
@@ -94,18 +105,12 @@ ub_stop:                    ; --
     state 0                 ; num
     msg 0                   ; num cust
     send -1                 ; --
-    push std.sink_beh       ; sink_beh
-    beh 0                   ; --
+    push #?                 ; #?
+    push std.sink_beh       ; #? sink_beh
+    beh -1                  ; --
     ref std.commit
 
 ; Boot code runs when the module is loaded (but not when imported).
-
-try_me:                     ; (sp . env) <- (cust . args)
-    state 0                 ; (sp . env)
-    part -1                 ; ...
-    state 1                 ; ... sp
-    part -1                 ; ... ...
-    end commit
 
 boot:                       ; _ <- {caps}
     msg 0                   ; {caps}
@@ -190,14 +195,12 @@ boot:                       ; _ <- {caps}
     send -1                 ; debug_dev
     if std.commit           ; -- early exit
 
-;    push try_me         ; ... try_me
-;    new 0               ; ... try_me.()
-;    send 0              ; ...
-
     ; start counting forever
-;    push count_to       ; ... count_to
-;    new 0               ; ... count_to.()
-;    send 0              ; ...
+;    push #?             ; ... #?
+;    push #?             ; ... #? #?
+;    push count_to       ; ... #? #? count_to
+;    new -1              ; ... #? count_to.#?
+;    send -1             ; ...
 
     ref std.commit
 
